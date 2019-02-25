062831c8ba8d62d8daade4bd091a1a0e02d5c6e6
Fabio Silva <fabio.silva@irdeto.com>
2019-02-25 16:57:36 +0100

    WIP

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 const express = require('express');
+const aws4 = require('aws4');
+const https = require('https');
+const xml2js = require('xml2js');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello world!'));
+
+app.get('/', (req, res) => {
+    res.send('AWIS Json API');
+});
+
+app.get('*', (req, res) => {
+    const path = req.originalUrl;
+
+    const opts = {
+        service: 'awis',
+        region: 'us-west-1',
+        host: 'awis.us-west-1.amazonaws.com',
+        path,
+    };
+
+    aws4.sign(opts);
+    https.get(opts, (r) => {
+        let body = '';
+
+        r.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        r.on('end', () => {
+            xml2js.parseString(body, (err, result) => {
+                console.log('Error: ', err);
+                console.log(result);
+            });
+        });
+    }).on('error', (e) => {
+        console.log('Got an error: ', e);
+    });
+
+    res.send('AWIS Json API *');
+});
+
 module.exports = app;
